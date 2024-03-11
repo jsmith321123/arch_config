0b11ca0a545291484b1da34c419ef8f3625a2155
Josh Smith <joshuaellissmith26@gmail.com>
2024-03-11 17:49:07 +0200

    new nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,16 @@
--- vim.opt.colorcolumn="80"
+vim.opt.colorcolumn="80"
 vim.opt.textwidth=80
 
--- vim.cmd([[
--- highlight ColorColumn ctermbg=8
--- ]])
+vim.cmd([[
+highlight ColorColumn ctermbg=8
+]])
 
 vim.opt.tabstop=2
 vim.opt.shiftwidth=2
 vim.opt.autoindent=true
 vim.opt.smartindent=true
 vim.opt.expandtab=true
-vim.opt.wrap=true
-vim.opt.breakindent=true
+vim.opt.wrap=false
 
 
 vim.api.nvim_set_keymap("n", "<C-Left>", ":tabprev<CR>", { noremap = true })
@@ -21,6 +20,8 @@ vim.api.nvim_set_keymap("n", "<C-Down>", ":tabclose<CR>", { noremap = true })
 
 vim.api.nvim_set_keymap("n", "<C-P>", ":Telescope find_files<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-F>", ":Telescope live_grep<CR>", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "<C-K>", ":call CocActionAsync('doHover')<CR>", { noremap = true })
 
 vim.cmd([[
 call plug#begin()
