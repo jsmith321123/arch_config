24a9206c55f5adbfd1002abccc35f93499400319
Josh Smith <joshuaellissmith26@gmail.com>
2024-05-09 17:51:52 +0300

    new keybindings for init lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,16 @@
-vim.opt.colorcolumn="80"
+--vim.opt.colorcolumn="80"
 vim.opt.textwidth=80
 
-vim.cmd([[
-highlight ColorColumn ctermbg=8
-]])
+-- vim.cmd([[
+-- highlight ColorColumn ctermbg=8
+-- ]])
 
 vim.opt.tabstop=2
 vim.opt.shiftwidth=2
 vim.opt.autoindent=true
 vim.opt.smartindent=true
 vim.opt.expandtab=true
-vim.opt.wrap=false
+vim.opt.wrap=true
 
 
 vim.api.nvim_set_keymap("n", "<C-Left>", ":tabprev<CR>", { noremap = true })
@@ -22,6 +22,9 @@ vim.api.nvim_set_keymap("n", "<C-P>", ":Telescope find_files<CR>", { noremap = t
 vim.api.nvim_set_keymap("n", "<C-F>", ":Telescope live_grep<CR>", { noremap = true })
 
 vim.api.nvim_set_keymap("n", "<C-K>", ":call CocActionAsync('doHover')<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-J>", "<Plug>(coc-codeaction-cursor)", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "<Leader>p", ":Neoformat<CR>", { noremap = true })
 
 vim.cmd([[
 call plug#begin()
