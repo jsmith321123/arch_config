4738be2f8c79f96b59f634ba8d0815fc9530becc
Josh Smith <joshuaellissmith26@gmail.com>
2025-05-02 16:18:09 +0100

    waybar and rounding

--- waybar/weather.py
+++ waybar/weather.py
@@ -9,14 +9,15 @@ async def getweather():
     async with python_weather.Client(unit=python_weather.METRIC) as client:
         weather = await client.get('Hampton-Hill')
 
-        print(weather)
+        print(weather.kind.emoji)
 
         sys.stdout.write("\n")
         sys.stdout.flush()
         sys.stdout.write(json.dumps({
             "text": 
                 str(weather.temperature) + " " 
-                + weather.description,
+                + weather.kind.emoji,
+                #+ weather.description,
             "class": "custom-weather",
             "alt": "weather"
         }))
