f3026a25c2e44e71946caf854c1e441143383b12
Josh Smith <joshuaellissmith26@gmail.com>
2025-05-02 15:16:08 +0100

    update wofi and waybar

--- waybar/weather.py
+++ waybar/weather.py
@@ -7,14 +7,16 @@ import json
 
 async def getweather():
     async with python_weather.Client(unit=python_weather.METRIC) as client:
-        weather = await client.get('Jyvaskyla')
+        weather = await client.get('Hampton-Hill')
+
+        print(weather)
 
         sys.stdout.write("\n")
         sys.stdout.flush()
         sys.stdout.write(json.dumps({
             "text": 
-                str(weather.current.temperature) + " " 
-                + weather.current.description,
+                str(weather.temperature) + " " 
+                + weather.description,
             "class": "custom-weather",
             "alt": "weather"
         }))
