75f222d05c49e00fa187e546c7c81d513599a880
Josh Smith <jsmith@Josh-Gaming.Home>
2025-06-17 12:52:52 +0100

    i3 updates

--- i3/bg-slideshow.sh
+++ i3/bg-slideshow.sh
@@ -1,5 +1,6 @@
-while true;
-do
-	feh --randomize --bg-fill /home/jsmith/.config/wallpapers/*
-	sleep 300
-done
+#while true;
+#do
+#	feh --randomize --bg-fill /home/jsmith/.config/wallpapers/*
+#	sleep 300
+#done
+feh --bg-fill /home/jsmith/.config/wallpapers/0023-a895a.jpg
