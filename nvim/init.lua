5e2856f4847033c0546f62b169017bb9aa87417a
Josh Smith <joshuaellissmith26@gmail.com>
2026-07-10 15:23:03 +0100

    fix random paste issue

--- nvim/init.lua
+++ nvim/init.lua
@@ -147,7 +147,7 @@ cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      vim.fn["vsnip#anvim redundant whitespace highlightnonymous"](args.body) -- For `vsnip` users.
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
     end,
   },
   window = {
